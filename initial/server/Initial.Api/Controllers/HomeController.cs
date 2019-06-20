90e83d461c31fabc0aa4bd9efae00cec6716d733
Jordan Jordanov <d_yugioh@abv.bg>
2019-06-20 15:26:56 +0300

    init

---
+++ initial/server/Initial.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Initial.Api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+	    [HttpGet]
+	    public ActionResult<string> Get() 
+		    => "Welcome to my initial docker supported API.";
+    }
+}
